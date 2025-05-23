4fad612893fe08fc2819d72ad8c90d1cf9e2a9b5
yousef romany30 <yousefromany527@gmail.com>
2025-05-23 10:06:07 +0000

    ال direction بتاع ال switcher معكوس

--- src/components/ui/switch.tsx
+++ src/components/ui/switch.tsx
@@ -20,7 +20,10 @@ const Switch = React.forwardRef<
   >
     <SwitchPrimitives.Thumb
       className={cn(
-        "pointer-events-none block h-5 w-5 rounded-full bg-background shadow-lg ring-0 transition-transform data-[state=checked]:translate-x-5 data-[state=unchecked]:translate-x-0"
+        "pointer-events-none block h-5 w-5 rounded-full bg-background shadow-lg ring-0 transition-transform",
+        "data-[state=unchecked]:translate-x-0",
+        "ltr:data-[state=checked]:translate-x-5",
+        "rtl:data-[state=checked]:-translate-x-5"
       )}
     />
   </SwitchPrimitives.Root>
